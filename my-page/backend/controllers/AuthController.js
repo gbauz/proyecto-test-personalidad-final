cb7136d69bda9aad121fe0785e01d74813158cb8
Camilo <zcamilozcastillo.com>
2025-07-19 20:46:42 -0500

    finalizad24455555orr

--- my-page/backend/controllers/AuthController.js
+++ my-page/backend/controllers/AuthController.js
@@ -187,7 +187,7 @@ export const forgotPassword = async (req, res) => {
       },
     });
 
-    const resetLink = `https://proyecto-test-personalidad-frontend.vercel.app/reset-password?token=${token}`;
+    const resetLink = `https://lively-torte-99ea96.netlify.app/reset-password?token=${token}`;
 
     const html = `
       <h3>Recuperación de contraseña</h3>
